3bb6f581169ef4f408df0bbdeeb52031f13b81ba
Mirvdijk <75266009+Mirvdijk@users.noreply.github.com>
2023-05-12 19:14:28 +0200

    Adding package
    Package includes setup and license

---
+++ Shazam/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name="Shazam",
+    version="0.0.1",
+    author="Mirthe van Dijk",
+    author_email="mirthevandijkanvr@gmail.com",
+    python_requires=">=3.6",
+    packages=setuptools.find_packages()
+)
